fe39ce9d4ec33075c1bd60362ec8bb012f689590
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-11-12 02:49:43 +0700

    update

--- src/main/java/com/service/disroauthservice/rest/AuthController.java
+++ src/main/java/com/service/disroauthservice/rest/AuthController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/auth-service")
 public class AuthController {
 
